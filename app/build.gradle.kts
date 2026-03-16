3d52a00eb56e2a280e57e46c522e9c858d85d3f2
bloodmoon <flash@flashbox.org>
2026-03-16 14:13:44 +0100

    first app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-serialization")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
